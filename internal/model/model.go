744f754788791cca0396ee188878cf5795ec1b91
azuki774 <azuki774s@gmail.com>
2022-09-19 01:08:26 +0900

    add create result model

--- internal/model/model.go
+++ internal/model/model.go
@@ -5,8 +5,8 @@ import "time"
 type CSVData [][]string
 
 type DailyRecord struct {
-	RecordDate        time.Time // 取り込み日付
-	FundName          string    // ファンド名
+	RecordDate        time.Time `gorm:"primaryKey"` // 取り込み日付
+	FundName          string    `gorm:"primaryKey"` // ファンド名
 	Amount            int       // 数量 47947
 	AcquisitionPrice  int       // 取得単価 12796
 	NowPrice          int       // 現在値 12864
@@ -15,4 +15,11 @@ type DailyRecord struct {
 	Profit            float64   // 損益 +326.03
 	ProfitRatio       float64   // 損益（％） +0.53
 	Valuation         float64   // 評価額 61679.02
+}
+
+type CreateRecordResult struct {
+	CreatedNumber int `json:"created_number,omitempty"`
+	UpdatedNumber int `json:"updated_number,omitempty"`
+	SkippedNumber int `json:"skipped_number,omitempty"`
+	FailedNumber  int `json:"failed_number,omitempty"`
 }
